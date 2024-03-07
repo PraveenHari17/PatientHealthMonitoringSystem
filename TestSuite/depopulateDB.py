81c2e3b3f2cea35a6786f3a70fa942c33cf34836
R78942 <56316956+R78942@users.noreply.github.com>
2024-03-07 13:58:24 -0500

    [Russell Radko] feat: add more entries during database population

--- TestSuite/depopulateDB.py
+++ TestSuite/depopulateDB.py
@@ -1,14 +1,15 @@
 import pyrebase 
 
 def main(db, userToken):
-	id = 100
-	#Patients
-	db.child("patients").child(id).remove(userToken)
-	#Actuators
-	db.child("actuators").child(id).remove(userToken)
-	#Rules
-	db.child("rules").child(id).remove(userToken)
-	#Sensor
-	db.child("sensors").child(id).remove(userToken)
-	#Sensor Data
-	db.child("sensorData"+id).remove(userToken)
+	ids = [100, 101, 102]
+	for id in ids:
+		#Patients
+		db.child("patients").child(id).remove(userToken)
+		#Actuators
+		db.child("actuators").child(id).remove(userToken)
+		#Rules
+		db.child("rules").child(id).remove(userToken)
+		#Sensor
+		db.child("sensors").child(id).remove(userToken)
+		#Sensor Data
+		db.child("sensorData"+id).remove(userToken)
--- TestSuite/populateDB.py
+++ TestSuite/populateDB.py
@@ -1,20 +1,21 @@
 import pyrebase 
 
 def main(db, userToken):
-	id = 100
-	#Patients
-	db.child("patients").child(id).child("name").set("Eva", userToken)
-	#Actuators
-	db.child("actuators").child(id).child("name").set("Heater", userToken)
-	#Rules
-	db.child("rules").child(id).child("name").set("Temperature above 20C", userToken)
-	db.child("rules").child(id).child("sensor").set("0", userToken)
-	db.child("rules").child(id).child("value").set("20", userToken)
-	db.child("rules").child(id).child("comparison").set("<", userToken)
-	db.child("rules").child(id).child("actuator").set("0", userToken)
-	db.child("rules").child(id).child("action").set("1", userToken)
-	#Sensor
-	db.child("sensors").child(id).child("name").set("Room Temperature", userToken)
-	db.child("sensors").child(id).child("units").set("Celcius", userToken)
-	#Sensor Data
-	db.child("sensorData"+id).child("1709836639").child("value").set("19", userToken)
+	ids = [100, 101, 102]
+	for id in ids:
+		#Patients
+		db.child("patients").child(id).child("name").set("Eva", userToken)
+		#Actuators
+		db.child("actuators").child(id).child("name").set("Heater", userToken)
+		#Rules
+		db.child("rules").child(id).child("name").set("Temperature above 20C", userToken)
+		db.child("rules").child(id).child("sensor").set("0", userToken)
+		db.child("rules").child(id).child("value").set("20", userToken)
+		db.child("rules").child(id).child("comparison").set("<", userToken)
+		db.child("rules").child(id).child("actuator").set("0", userToken)
+		db.child("rules").child(id).child("action").set("1", userToken)
+		#Sensor
+		db.child("sensors").child(id).child("name").set("Room Temperature", userToken)
+		db.child("sensors").child(id).child("units").set("Celcius", userToken)
+		#Sensor Data
+		db.child("sensorData"+id).child("1709836639").child("value").set("19", userToken)
