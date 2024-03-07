d118b25c86143fb696d86b2378cc4a5119783e93
R78942 <56316956+R78942@users.noreply.github.com>
2024-03-07 13:55:33 -0500

    [Russell Radko] feat: test suite database population scripts

--- TestSuite/depopulateDB.py
+++ TestSuite/depopulateDB.py
@@ -1,4 +1,14 @@
 import pyrebase 
 
 def main(db, userToken):
-	
+	id = 100
+	#Patients
+	db.child("patients").child(id).remove(userToken)
+	#Actuators
+	db.child("actuators").child(id).remove(userToken)
+	#Rules
+	db.child("rules").child(id).remove(userToken)
+	#Sensor
+	db.child("sensors").child(id).remove(userToken)
+	#Sensor Data
+	db.child("sensorData"+id).remove(userToken)
--- TestSuite/populateDB.py
+++ TestSuite/populateDB.py
@@ -1,19 +1,20 @@
 import pyrebase 
 
 def main(db, userToken):
+	id = 100
 	#Patients
-	db.child("patients").child("0").child("name").set("Eva", userToken)
+	db.child("patients").child(id).child("name").set("Eva", userToken)
 	#Actuators
-	db.child("actuators").child("0").child("name").set("Heater", userToken)
+	db.child("actuators").child(id).child("name").set("Heater", userToken)
 	#Rules
-	db.child("rules").child("0").child("name").set("Temperature above 20C", userToken)
-	db.child("rules").child("0").child("sensor").set("0", userToken)
-	db.child("rules").child("0").child("value").set("20", userToken)
-	db.child("rules").child("0").child("comparison").set("<", userToken)
-	db.child("rules").child("0").child("actuator").set("0", userToken)
-	db.child("rules").child("0").child("action").set("1", userToken)
+	db.child("rules").child(id).child("name").set("Temperature above 20C", userToken)
+	db.child("rules").child(id).child("sensor").set("0", userToken)
+	db.child("rules").child(id).child("value").set("20", userToken)
+	db.child("rules").child(id).child("comparison").set("<", userToken)
+	db.child("rules").child(id).child("actuator").set("0", userToken)
+	db.child("rules").child(id).child("action").set("1", userToken)
 	#Sensor
-	db.child("actuators").child("0").child("name").set("Room Temperature", userToken)
-	db.child("actuators").child("0").child("units").set("Celcius", userToken)
+	db.child("sensors").child(id).child("name").set("Room Temperature", userToken)
+	db.child("sensors").child(id).child("units").set("Celcius", userToken)
 	#Sensor Data
-	db.child("sensorData0").child("1709836639").child("value").set("19", userToken)
+	db.child("sensorData"+id).child("1709836639").child("value").set("19", userToken)
